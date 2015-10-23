59e9aaea0cbc90b72330a8f21cd20fba9405cf40
Seed <seed@ubuntu.(none)>
2015-10-23 02:06:15 -0700

    readme.txt

---
+++ test/payloads/payload1.py
@@ -0,0 +1,11 @@
+#coding:UTF-8
+'''攻击载荷1'''
+shellcode = "payload1"
+
+class payload(object):
+	'payload1'
+#	__init__(self):
+#		self.data = shellcode
+
+if __name__=='__main__':
+	print __doc__
---
+++ test/payloads/payload2.py
@@ -0,0 +1,5 @@
+#coding:UTF-8
+'''攻击载荷2'''
+
+if __name__=='__main__':
+	print __doc__
