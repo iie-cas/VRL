25985dc8dff578c973e7b8f62d25ae519d39d70f
readm <lijinfeng@iie.ac.cn>
2016-08-15 11:33:53 +0800

    trying fix a bug

--- VRL.py
+++ VRL.py
@@ -1,7 +1,10 @@
 #! /usr/bin/python
 #coding:utf-8
 import sys
+import os
 from modules import *
+import cmd
+'''
 #test
 #sys.path.append(r"exploits")
 #ex=__import__('stack_overflow',globals(),locals(),['run'])
@@ -10,9 +13,91 @@ from modules import *
 #exp.check()
 #exp.run()
 #print ex.test.b
+'''
 
 
+class ui(cmd.Cmd):
+    prompt = '(VRL)'
+    intro = 'Welcome to VRL'
+    global exp,vul,exploits,vulnerabilities
 
+    def do_reload(self,line):
+        '''Reload all exploits,vulnerabilities,payloads,etc.
+        When VRL started, loading is done.
+        So you only need to use this when you add something new and do not want to restart VRL.'''
+        for type in ['exploits','vulnerabilities']:
+            subpath=os.path.join(os.curdir,type)
+            for name in os.listdir(subpath):
+                if os.path.isdir(os.path.join(subpath,name)):
+                    if 'run.py' in os.listdir(os.path.join(subpath,name)):
+                        if type=='exploits': exploits.append(str(name))
+                        if type=='vulnerabilities': vulnerabilities.append(str(name))
+
+
+    def do_show(self,type):
+        '''Show all exploits|vulnerabilities|payload
+        format: list exploit|vulnerabilities|payload (e|v|p for short.)'''
+        if type:
+            path={'e':exploits,'v':vulnerabilities}#,'p':payload}
+            if type[0] in path.keys():
+                for i in path[type[0]]:
+                    print i
+        else:
+            print "Wrong format!"
+
+    def do_usevul(self,name):
+        '''Use a vulnerability
+        format: usevul vulnerability_name'''
+        global vul
+        try:
+            sys.path.append(os.path.join(os.curdir,'vulnerabilities'))
+            temp=__import__(name+'.run')
+            vul=temp.run.Vulnerability()
+            sys.path.remove(os.path.join(os.curdir,'vulnerabilities'))
+        except Exception,e:
+            print e
+
+    def do_useexp(self,name):
+        '''Use an exploit
+        format: useexp exploit_name'''
+        global exp
+        sys.path.append(os.path.join(os.curdir,'exploits'))
+        temp=__import__(name+'.run')
+        exp=temp.run.Exploit()
+        sys.path.remove(os.path.join(os.curdir,'exploits'))
+
+    def do_runvul(self,line):
+        '''Run the vulnerability using'''
+        if vul:
+            vul.run()
+        else:
+            print 'Error: No vulnerability using.'
+
+    def do_runexp(self,line):
+        '''Run the exploit using'''
+        if exp:
+            exp.run()
+        else:
+            print 'Error: No exploit using.'
+
+    def do_qrun(self,vul):
+        '''Quick run a vulnerability and it's default exploit with default options
+        format: qrun []'''
+    def do_EOF(self,line):
+        return True
+
+#list of exp & vul
+exploits=[]
+vulnerabilities=[]
+#exp & vul using
+exp=[]
+vul=[]
+
+VRLui=ui()
+VRLui.do_reload('')
+VRLui.cmdloop()
+
+'''
 options={'dIP':"127.0.0.1", 'dPort':"11111", 'exploit':"exploit6", 'payload':""}
 
 def show_exploits(args=[]):      #命令show参数对应的函数定义
@@ -86,3 +171,4 @@ while True:
     commands[command[0]](command[1:])
 
         
+'''
