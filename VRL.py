97497cbf0574900fff81cb764801a2d22cce4e36
readm <lijinfeng@iie.ac.cn>
2016-08-15 15:33:47 +0800

    implement show options, quick run, use vul/exp, run/stop vul/exp,reload, and quit.

--- VRL.py
+++ VRL.py
@@ -2,10 +2,12 @@
 #coding:utf-8
 import sys
 import os
+import cmd
+
+
 from modules import *
 import vulnerabilities
 import exploits
-import cmd
 
 
 class ui(cmd.Cmd):
@@ -29,7 +31,6 @@ class ui(cmd.Cmd):
                         if type=='exploits': exploit_list.append(str(name))
                         if type=='vulnerabilities': vulnerability_list.append(str(name))
 
-
     def do_show(self,type):
         '''Show all exploits|vulnerabilities|payload
         format: list exploit|vulnerabilities|payload (e|v|p for short.)'''
@@ -42,10 +43,12 @@ class ui(cmd.Cmd):
                 if vul or exp:
                     if vul:
                         print "Vulnerability options:"
-                        print vul.options
+                        for (key,value) in vul.options.items():
+                            print key,':',value
                     if exp:
                         print "Exploit options:"
-                        print exp.options
+                        for (key,value) in exp.options.items():
+                            print key,':',value
                 else:
                     print 'Error: No vulnerability or exploit using.'
             else:
@@ -61,6 +64,11 @@ class ui(cmd.Cmd):
             _temp=__import__('vulnerabilities.'+name+'.run',globals(),locals(),fromlist=['Vulnerability'])
             Vulnerability = _temp.Vulnerability
             vul=Vulnerability()
+            print 'Vulnerability Loaded'
+            if hasattr(vul,'exploit') and not exp:
+                c = raw_input("This vulnerability has a default exploit, use the exploit?(y/n):(y)")
+                if not c or c[0] != 'n':
+                    self.do_useexp(vul.exploit)
         except Exception,e:
             print e
 
@@ -72,6 +80,11 @@ class ui(cmd.Cmd):
             _temp=__import__('exploits.'+name+'.run',globals(),locals(),fromlist=['Exploit'])
             Exploit= _temp.Exploit
             exp=Exploit()
+            print 'Exploit Loaded'
+            if hasattr(exp,'vulnerability') and not vul:
+                c = raw_input("This exploit has a default vulnerability, use the exploit?(y/n):(y)")
+                if not c or c[0] != 'n':
+                    self.do_usevul(exp.vulnerability)
         except Exception,e:
             print e
 
@@ -85,14 +98,47 @@ class ui(cmd.Cmd):
     def do_runexp(self,line):
         '''Run the exploit using'''
         if exp:
-            exp.run()
+            exp.stop()
         else:
             print 'Error: No exploit using.'
 
-    def do_qrun(self,vul):
+    def do_stopvul(self,line):
+        '''Stop the vulnerability using'''
+        if vul:
+            if hasattr(vul,'stop'):
+                vul.stop()
+            else:
+                print 'Error: This vulnerability cannot stop.'
+        else:
+            print 'Error: No vulnerability using.'
+
+    def do_stopexp(self,line):
+        '''Stop the exploit using'''
+        if exp:
+            if hasattr(exp,'stop'):
+                exp.stop()
+            else:
+                print 'Error: This exploit cannot stop.'
+        else:
+            print 'Error: No exploit using.'
+
+    def do_qrun(self,name):
         '''Quick run a vulnerability and it's default exploit with default options
         format: qrun []'''
+        if not vul and not exp:
+            if not name:
+                print "No vulnerability or exploit using, enter a name to quick run."
+                return
+            self.do_usevul(name)
+        self.do_runvul('')
+        self.do_runexp('')
+
+    def do_q(self,line):
+        '''Quit VRL.'''
+        self.do_EOF(line)
+
     def do_EOF(self,line):
+        '''Quit VRL'''
         return True
 
 #list of exp & vul
@@ -106,78 +152,3 @@ VRLui=ui()
 VRLui.do_reload('')
 VRLui.cmdloop()
 
-'''
-options={'dIP':"127.0.0.1", 'dPort':"11111", 'exploit':"exploit6", 'payload':""}
-
-def show_exploits(args=[]):      #命令show参数对应的函数定义
-    for item in exploits.__all__:
-        print "\t",item,": ",eval(item).__doc__
-def show_payloads(args=[]):
-    for item in payloads.__all__:
-        print "\t",item,": ",eval(item).__doc__
-def show_options(args=[]):
-    for key in sorted(options.keys()):
-        print "\t",key,": ",options[key]
-
-
-shows={'exploits':show_exploits, 'payloads':show_payloads, 'options':show_options} #命令show的参数
-
-
-def help(args=[]):
-    print "\thelp: 帮助信息"
-    print "\tquit: 退出"
-    print "\treload: 重新加载exploits和payloads模块"
-    print "\tshow exploits|payloads|options: 显示相应的信息"
-    print "\t     exploits: 显示所有的exploits模块"
-    print "\t     payloads: 显示所有的payloads模块"
-    print "\t     options: 显示test所需要设置的参数"
-    print "\tset dIP|dPort|exploit|payload arg: 设置test所需要设置的参数"
-    print "\t    dIP: 设置目标IP"
-    print "\t    dPort: 设置目标端口"
-    print "\t    exploit: 所选用的exploit模块"
-    print "\t    payload: 所选用的payload模块"
-    print "\tui: 设置完参数后进行测试"
-def show(args=[]):
-    if len(args)!=1 or args[0] not in shows:
-        print "\tInvalid args!"
-        return
-    shows[args[0]]()
-def setup(args=[]):
-    if len(args)!=2 or args[0] not in options:
-        print "\tInvalid args!"
-        return
-    options[args[0]]=args[1]
-def load(args=[]):
-    for e in exploits.__all__:
-        reload(eval(e))
-    for p in payloads.__all__:
-        reload(eval(p))
-def attack(args=[]):
-    print "\tNow testing..."
-    #eval("from exploits import "+options['exploit']+" as e")
-    e = eval(options['exploit'])
-    t = e.exploit(options)
-    try:
-        t.test()
-    except BaseException, e:
-        print "ERROR:", e
-        print "Please reset your options."
-
-commands={'help':help, 'show':show, 'set':setup, 'reload':load, 'ui':attack} #所有命令
-
-while True:
-    command = raw_input('ui >> ').split()
-    if len(command) == 0:
-        print "\tMissing command!"
-        print "\tEnter 'help' for help."
-        continue
-    if command[0] in ['quit', 'q']:
-        sys.exit()
-    if command[0] not in commands:
-        print "\tInvalid command!"
-        print "\tEnter 'help' for help."
-        continue
-    commands[command[0]](command[1:])
-
-        
-'''
