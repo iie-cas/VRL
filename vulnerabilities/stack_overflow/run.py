d87a86aea3eea95f31947193bd86d07cbcdbac8c
GYJQTYL2 <1348434634@qq.com>
2016-08-16 01:48:45 -0700

    add new run.py

--- vulnerabilities/stack_overflow/run.py
+++ vulnerabilities/stack_overflow/run.py
@@ -1,31 +1,30 @@
 #! /usr/bin/python
 #coding:utf-8
+#from modules import vulnerability
 import os
-from modules import vulnerability
-
-class Vulnerability(vulnerability.VRL_Vulnerability):
+class Vulnerability(object):
     def __init__(self):
         '''Add information of your vulnerability here'''
         self.name = 'stack_overflow'
         self.info = 'information'
         self.options={'dIP' : '127.0.0.1',
-                      'dPort' : '12345'}
+                      'dPort' : '34566'}
         self.exploit = 'stack_overflow'
 
     def run(self):
         '''Run your vulnerability here, if this script could success, the VRL can run it.
         When the vulnerability run, follow the options.'''
-        print 'run your vulnerability here'
+        p = os.popen('./ggtest '+ self.options['dPort'],'r')
+       # print p.write()
+        print p.read()
 
 '''Bellowing is default, simply ignore it.'''
 if __name__ == "__main__":
-    if '__init__.py' not in os.listdir(os.curdir):
-        os.mknod('__init__.py')
     vul = Vulnerability()
     print 'Vulnerability: ',vul.name,' \n'
-    print 'Checking:\n'
-    if vul.check():
-        print 'Running:\n'
-        vul.run()
+   # print 'Checking:\n'
+    #if vul.check():
+     #   print 'Running:\n'
+    vul.run()
 
 
